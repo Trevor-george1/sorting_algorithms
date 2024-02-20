eed9bec4b0392da76502570cf09d750f7769d3bd
Trevor <Trevormahinda@gmai.com>
2024-02-20 15:31:21 +0300

    create new

--- sort.h
+++ sort.h
@@ -70,4 +70,10 @@ void cocktail_sort_list(listint_t **list);
 int get_max(int *array, int size);
 
 void counting_sort(int *array, size_t size);
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+
+void merge_sort(int *array, size_t size);
 #endif
